7146b4acd45006cecbee47ca1c0f3d079733dd99
Ritesh Karankal <ritesh.karankal200@gmail.com>
2025-07-01 22:19:32 +0530

    connect go with mysql / build a book management system

---
+++ go-bookstore/go.mod
@@ -0,0 +1,15 @@
+module github.com/ritesh-karankal/go-bookstore
+
+go 1.24.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1
+)
---
+++ go-bookstore/pkg/config/app.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, port, name)
+	d, err := gorm.Open("mysql", dsn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
